d9f78709068a147de589fcd9e00843199fd1bfbc
syahmi <ggsyahmi20@gmail.com>
2024-01-04 21:57:37 +0800

    add delete method

--- index.php
+++ index.php
@@ -16,6 +16,13 @@
             header('location: index.php');
         }
     }
+
+    // delete task
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+        mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+        header('location: index.php');
+    }
 ?>
 
 <!DOCTYPE html>
